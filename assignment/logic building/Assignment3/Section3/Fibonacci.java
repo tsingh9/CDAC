6cb738650d3a6def1c51e212624b49f6f508e7fa
tsingh9 <126694058+tsingh9@users.noreply.github.com>
2024-09-02 01:23:48 +0530

    Update Fibonacci.java

--- assignment/logic building/Assignment3/Section3/Fibonacci.java
+++ assignment/logic building/Assignment3/Section3/Fibonacci.java
@@ -1,29 +1,17 @@
-import java.io.*;
-import java.util.*;
-
-public class Q1{
+public class Fibonacci{
 public static void main(String[] args) {
+ int f=0;
+ int n=1;
+ System.out.print("Fibonacci series: ");
+while(f<=21){
+System.out.print(" "+f);
+
+int a=f;
+f=a+n;
+n=a;
+
+}
+}
+}
+
 	
-	int count=0;
-	System.out.println("Enter a number: ");
-	Scanner sc =new Scanner(System.in);
-	int n=sc.nextInt();
-	
-	if(n==0||n==1||n==2)
-	{
-		System.out.println("It is a prime number ");
-		
-	}
-	else {
-		for(int i=2;i<=n;i++)
-		{ if(n%i==0)
-			count++
-			
-		}
-		if(count>1)
-		{
-		System.out.println("It is a prime number ");
-		}
-		else
-			System.out.println("It is not a prime number ");
-	}
