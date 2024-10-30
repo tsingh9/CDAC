1fe59cc03c13206b2a7b7a2b71ac8e78e3c3815f
tsingh9 <126694058+tsingh9@users.noreply.github.com>
2024-10-30 14:31:59 +0530

    Update Q2.js

--- assignment/WPT/Assign4/Q2.js
+++ assignment/WPT/Assign4/Q2.js
@@ -1,7 +1,9 @@
-
 const a=[1,2,3,4,5]
     forEachElement(a,(a)=>{for(i=0;i<a.length;i++){
         console.log(a[i]*2);
     }});
 
-   
+   function forEachElement(a,callback){
+    callback(a)
+
+   }
